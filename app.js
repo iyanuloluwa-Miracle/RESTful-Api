45f09ad270ce417b92b77a4c15d77cf7bca08782
iyanuloluwa-Miracle <iyanudina@gmail.com>
2022-12-31 07:41:01 +0100

    added some comment to my work

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const express = require('express')
+const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-const app = express()
-
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,30 +14,104 @@ mongoose.connect("mongodb://localhost:27017/nikiDB", { useNewUrlParser: true });
 
 const articlesSchema = {
   title: String,
-  content: String
-}
+  content: String,
+};
 
-const Article = mongoose.model("Article", articlesSchema)
+const Article = mongoose.model("Article", articlesSchema);
 //Restful Get request//
-app.get('/articles', function(req,res){
-  Article.find(function(err, foundArticles){
-    if (!err){
-      res.send(foundArticles);
 
-    }else{
-      res.send(err)
-    }
-    //console.log(foundArticles)
-    //res.send(foundArticles)
+//instead of repeatedly using aricles we could make our life easier by doing this
 
-
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+      //console.log(foundArticles)
+      //res.send(foundArticles)
+    });
   })
-})
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new article");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Successfully deleted all articles ");
+      } else {
+        res.send(err);
+      }
+    });
+  });
 
-app.get('/', (req, res) => {
-  res.send('Api Ready!')
-})
+
+
+
+////////  A request targeting a specific article ////////////////////////////
+
+//Get Request
+//app.get('/articles', function(req,res){
+//  Article.find(function(err, foundArticles){
+//    if (!err){
+//      res.send(foundArticles);
+
+//    }else{
+//      res.send(err)
+//    }
+//    //console.log(foundArticles)
+//    //res.send(foundArticles)
+
+//})
+//})
+
+//Post Request
+//mostly used in form
+//app.post('/articles', function(req,res){
+
+//  const newArticle = new Article({
+//    title: req.body.title,
+//    content:req.body.content
+//  })
+//  newArticle.save(function(err){
+//    if(!err){
+//      res.send("Successfully added a new article")
+//    }else{
+//      res.send(err)
+//    }
+
+//  })
+
+//})
+
+//deleting
+//app.delete("/articles", function(req,res){
+//  Article.deleteMany(function(err){
+//    if(!err){
+//      res.send("Successfully deleted all articles ")
+//    }else{
+//      res.send(err)
+//    }
+//  })
+
+//})
+app.get("/", (req, res) => {
+  res.send("Api Ready!");
+});
 
 app.listen(3002, () => {
-  console.log('Example app listening on port 3002!')
-})
+  console.log("Example app listening on port 3002!");
+});
